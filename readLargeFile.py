583cb39c365338ad2dc4c56595e5dd2521e0876d
xiaofengleo <sclxf@163.com>
2015-02-23 20:15:21 -0800

    Extract the text section from each sequence.
    improve the previous version. Extract the text section from each
    sequence.

--- readLargeFile.py
+++ readLargeFile.py
@@ -1,7 +1,7 @@
 #Author:Xiaofeng Liao. Date:20 Feb 2015
 #A python script which can extract specific sequences from MAF files. A
 #MAF file usually larger than 10 GB. Specifically, this script scan the
-#file line by line, pick the lines starts with “s”, which means that
+#file line by line, pick the lines starts with s, which means that
 #line is a sequence. In addition, only the alignment block with exactly
 #7 sequences are extract. Any alignment with more than or less than 7
 #sentences are discarded. The extracted 7 sequences are printed line by
@@ -10,12 +10,21 @@
 import fileinput
 list = []
 for line in fileinput.input("hg38.7way.maf"):
-	if not line in ['\n']:
-		if line.startswith("s"):
-			list.append(line)
-	else:
+	if not line in ['\n']:#if the line is not empty, go on check it
+		if line.startswith("s"):#if it starts with s
+			#split the line and get the 7th section, the text
+			sections = line.split()
+			text = sections[6]#get the 7th section, 0 indexd
+			list.append(text)
+	else:#if the line is empty, then output the contents in the list
 		if len(list) == 7:
+			maxLen = 0
 			for item in list:
-				print item,
-			print '\n',
+				if len(item)>maxLen:
+						maxLen = len(item)
+			for item in list:
+				if len(item)<maxLen:
+					item.ljust(maxLen,'0')
+				print item
+			print '\n',#add a blank line as a separation
 		list = []
